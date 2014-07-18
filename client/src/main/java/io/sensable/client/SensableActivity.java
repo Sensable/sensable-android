70d4fa50d2a498216f3bea99922fdadb59c2cb3c
Simon Madine <simon@angryrobotzombie.com>
2014-07-18 21:36:51 +0200

    Displays value for each sample

--- client/src/main/java/io/sensable/client/SensableActivity.java
+++ client/src/main/java/io/sensable/client/SensableActivity.java
@@ -254,7 +254,13 @@ public class SensableActivity extends Activity {
             cal.setTime(thisSampleDate);
             Integer thisDay = cal.get(Calendar.DAY_OF_YEAR);
 
-            currentListList.get(currentIndex).add(cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE));
+            currentListList.get(currentIndex).add(cal.get(Calendar.YEAR)
+                                        + "-" + cal.get(Calendar.MONTH)
+                                        + "-" + cal.get(Calendar.DAY_OF_MONTH)
+                                        + " " + cal.get(Calendar.HOUR_OF_DAY)
+                                        + ":" + cal.get(Calendar.MINUTE)
+                                        + "  " + mSamples.get(i).getValue()
+                                        + "  " + sensable.getUnit());
 
             cal.setTime(nextSampleDate);
             int nextDay = cal.get(Calendar.DAY_OF_YEAR);
